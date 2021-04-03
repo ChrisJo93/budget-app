e02c5ddbf44bba267d3a50a84edebcf9e3d26346
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-04-03 01:40:18 -0500

    nav styled neatly. Needs highlighter but that can wait

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -3,23 +3,28 @@ import styles from '../../styles/Home.module.css';
 
 const Nav = () => {
   return (
-    <div className={styles.navContainer}>
-      <header>
-        <h2>Budgetier</h2>
-        <Link href="/">
-          <a>Overview</a>
-        </Link>
-        <Link href="/views/transactionView">
-          <a>Transactions</a>
-        </Link>
-        <Link href="/views/budgetView">
-          <a>Budget</a>
-        </Link>
-        <Link href="/">
-          <a>Logout</a>
-        </Link>
-      </header>
-    </div>
+    <header className={styles.navHeader}>
+      <div className={styles.navContainer}>
+        <div className={styles.navTitle}>
+          <h2>Budgetier</h2>
+        </div>
+
+        <div className={styles.navLinks}>
+          <Link href="/">
+            <a>Overview</a>
+          </Link>
+          <Link href="/views/transactionView">
+            <a>Transactions</a>
+          </Link>
+          <Link href="/views/budgetView">
+            <a>Budget</a>
+          </Link>
+          <Link href="/">
+            <a>Logout</a>
+          </Link>
+        </div>
+      </div>
+    </header>
   );
 };
 
