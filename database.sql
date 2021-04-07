c5b64e108cb166f7b76854cf99c7b54f1c7f1adf
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-04-07 02:41:37 -0500

    db tables created. Adding values

--- database.sql
+++ database.sql
@@ -1,2 +1,33 @@
--- Database needs a user, budget goals, months, transaction categories, and a few more things... still brainstorming.
--- lol this is why we build backend up when making an app. Just got a little carried away with this one.
+CREATE TABLE "user" (
+"id" SERIAL PRIMARY KEY,
+"username" varchar (200) UNIQUE NOT NULL,
+"password" varchar (200) NOT NULL,
+"phone" text,
+"email" text
+);
+
+CREATE TABLE "month" (
+"id" SERIAL PRIMARY KEY,
+"month_name" varchar (200)
+);
+
+CREATE TABLE "user_total_budget" (
+"id" SERIAL PRIMARY KEY,
+"total_amount" NUMERIC,
+"user_id" INT REFERENCES "user" NOT NULL,
+"month_id" INT REFERENCES "month"
+);
+
+CREATE TABLE "transaction_category" (
+"id" SERIAL PRIMARY KEY,
+"category_name" text
+);
+
+CREATE TABLE "user_budget_goal" (
+"id" SERIAL PRIMARY KEY,
+"goal_name" text,
+"user_id" INT REFERENCES "user",
+"month_id" INT REFERENCES "month",
+"category_id" INT REFERENCES "transaction_category",
+"amount" NUMERIC 
+);
