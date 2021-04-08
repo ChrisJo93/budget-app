382f6389d634e370dc1ad8d9a277bc607a381bfd
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-04-08 03:15:11 -0500

    I had way too much fun with this and haven't touched the database once lmao

--- server/server.js
+++ server/server.js
@@ -6,10 +6,6 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-// Nerd middleware
-const sound = require('sound-play');
-const path = require('path');
-
 app.prepare().then(() => {
   const server = express();
 
@@ -24,22 +20,28 @@ app.prepare().then(() => {
     return handle(req, res);
   });
 
-  function generateRandomNumber() {
-    return Math.floor(Math.random() * 2);
-  }
-
-  function randomSound(x) {
-    switch (x) {
-      case 1:
-        return './server/deetdoot.wav';
-      case 2:
-        return './server/auir.mp3';
-    }
-  }
-
   server.listen(3000, (err) => {
     if (err) throw err;
     sound.play(randomSound(generateRandomNumber()));
     console.log(`Joh'gaav?`);
   });
 });
+
+// Nerd middleware
+const sound = require('sound-play');
+
+function generateRandomNumber() {
+  return Math.floor(Math.random() * 2 + 1);
+}
+
+function randomSound(x) {
+  console.log(x);
+  switch (x) {
+    case 1:
+      return './server/deetdoot.wav';
+    case 2:
+      return './server/instruction.wav';
+    default:
+      return './server/deetdoot.wav';
+  }
+}
