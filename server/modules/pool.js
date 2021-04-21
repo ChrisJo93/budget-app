42fc724f1dbf66d6e3df37ef40cf1d05644f152d
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-04-20 23:24:19 -0500

    pool is bugging. The code worked before restarting the machine so maybe a default setting flipped?

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,28 +1,3 @@
-// const pg = require('pg');
-
-// let config = {};
-
-// config = {
-//   host: 'localhost',
-//   port: 5432,
-//   database: 'budget-data',
-//   max: 10,
-//   idleTimeoutMillis: 30000,
-// };
-
-// const pool = new pg.Pool(config);
-
-// pool.on('connect', () => {
-//   console.log(`My life for Auir`);
-// });
-
-// pool.on('error', (err) => {
-//   console.log('Unexpected error on idle client', err);
-//   process.exit(-1);
-// });
-
-// module.exports = pool;
-
 const pg = require('pg');
 const url = require('url');
 
