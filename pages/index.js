a849914db437c5bbf0c4981db8d0b8a22db46cef
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-04-21 19:46:53 -0500

    postgres issue solved. Huzzah. Simplifying this application to budget, expensive, workflow. Will scale up

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Nav from 'components/nav';
 import Footer from 'components/footer';
-import Test2 from 'class.dispatch';
 
 export default function Home() {
   return (
@@ -13,7 +12,6 @@ export default function Home() {
       </Head>
 
       <Nav />
-      <Test2 />
 
       <main className={styles.main}>
         <h3 className={styles.monthlyBudget}></h3>
