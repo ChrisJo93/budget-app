fc4b70a21b72bb62f29ea70f83df7dc3f1ac3ebd
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-05-01 02:17:37 -0500

    deleting goals and transaction views for now. Nuking original concept and starting again.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import '../styles/globals.css';
 import { wrapper } from 'redux/store';
-
+import Nav from 'components/nav';
 function MyApp({ Component, pageProps }) {
+  <Nav />;
   return <Component {...pageProps} />;
 }
 
