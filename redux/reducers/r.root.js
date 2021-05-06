f79129fc0cad863b91c47a321fac8402a92bafe5
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-05-06 13:17:07 -0500

    added transaction reducer

--- redux/reducers/r.root.js
+++ redux/reducers/r.root.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import month from './r.month';
 import wallet from './r.wallet';
+import transaction from './r.transaction';
 
 export default combineReducers({
   month,
   wallet,
+  transaction,
 });
---
+++ redux/reducers/r.transaction.js
@@ -0,0 +1,28 @@
+import { actionTypes } from '../actions';
+import { HYDRATE } from 'next-redux-wrapper';
+
+const initialState = {
+  error: null,
+  transactionList: [],
+  transactionByCategory: [],
+};
+
+const transaction = (state = initialState, action) => {
+  switch (action.type) {
+    case HYDRATE: {
+      return { ...state, ...action.payload };
+    }
+    case actionTypes.FAILURE:
+      return { ...state, ...{ error: action.error } };
+
+    case actionTypes.SET_MONTH_LIST:
+      return { ...state, transactionList: action.payload };
+
+    case actionTypes.SET_MONTH_GOAL:
+      return { ...state, transactionByCategory: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default transaction;
