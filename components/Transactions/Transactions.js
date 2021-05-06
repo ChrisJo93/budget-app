dbc2edccd3f98d6c407deec69c408b2378c2277e
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-05-06 12:59:29 -0500

    updated transaction router query

---
+++ components/Transactions/Transactions.js
@@ -0,0 +1,7 @@
+import { connect } from 'react-redux';
+
+const Transactions = (props) => {
+  return <div> I am a transaction table lol</div>;
+};
+
+export default connect((state) => state)(Transactions);
--- server/routes/transaction.router.js
+++ server/routes/transaction.router.js
@@ -3,7 +3,7 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  const query = `SELECT * FROM "user_transaction";`;
+  const query = `SELECT * FROM "transaction";`;
   pool
     .query(query)
     .then((result) => {
@@ -16,7 +16,7 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:category', (req, res) => {
-  const query = `SELECT * FROM "user_transaction"
+  const query = `SELECT * FROM "transaction"
   JOIN "transaction_category" on "user_transaction".category_id = "transaction_category".id
   WHERE "transaction_category".id = $1;`;
   pool
@@ -32,9 +32,9 @@ router.get('/:category', (req, res) => {
 
 router.post('/', (req, res) => {
   const items = req.body;
-  const query = `INSERT INTO "user_transaction" ("item_name", "amount", "date", "category_id", "user_id")
+  const query = `INSERT INTO "transaction" ("item_name", "amount", "date", "category_id", "user_id")
   VALUES ($1, $2, $3, $4, $5);`;
-  const query2 = `INSERT INTO "user_transaction" ("item_name", "amount", "category_id", "user_id")
+  const query2 = `INSERT INTO "transaction" ("item_name", "amount", "category_id", "user_id")
   VALUES ($1, $2, $3, $4);`;
 
   const promises = items.map((item) => {
@@ -79,7 +79,7 @@ router.post('/', (req, res) => {
 
 router.delete('/', (req, res) => {
   const items = req.body;
-  const query = `DELETE FROM "user_transaction" WHERE id = $1;`;
+  const query = `DELETE FROM "transaction" WHERE id = $1;`;
 
   const promises = items.map((item) => {
     pool.query(query, [item.id]).catch((err) => {
@@ -100,7 +100,7 @@ router.delete('/', (req, res) => {
 
 router.put('/', (req, res) => {
   const items = req.body;
-  const query = `UPDATE "user_transaction" SET 
+  const query = `UPDATE "transaction" SET 
     item_name = $1,
     amount = $2,
     date = $3,
