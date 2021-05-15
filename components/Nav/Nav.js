1ba20aaf5e0de4130adbe2745a6ae727ee19c67a
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-05-15 17:20:29 -0500

    nav positioned correctly. For the most part. Placing third background

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -4,33 +4,39 @@ import styles from './Nav.module.css';
 const Nav = () => {
   return (
     <header className={styles.nav_container}>
-      <div>
-        <h2>
-          <a
-            href="https://github.com/ChrisJo93"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Logo/Icon
-          </a>
-        </h2>
-      </div>
       <ul>
-        <li>
-          <Link href="/">
-            <a>Overview</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/">
-            <a>Space Holder</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/">
-            <a>Logout</a>
-          </Link>
-        </li>
+        <div>
+          <li>
+            <Link href="/">
+              <a>Overview</a>
+            </Link>
+          </li>
+        </div>
+        <div>
+          <li>
+            <Link href="/">
+              <a>Space Holder</a>
+            </Link>
+          </li>
+        </div>
+        <div>
+          <li>
+            <Link href="/">
+              <a>Logout</a>
+            </Link>
+          </li>
+        </div>
+        <div>
+          <li>
+            <a
+              href="https://github.com/ChrisJo93"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Logo/Icon
+            </a>
+          </li>
+        </div>
       </ul>
     </header>
   );
