d1c92993fec80f07508478af0dbb0bdec7a9b14f
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-05-23 21:06:52 -0500

    adding more to do lists for when I have more time.

--- server/routes/wallet.router.js
+++ server/routes/wallet.router.js
@@ -2,6 +2,12 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
+/* To Do:
+1. Post route for wallets.
+2. Put route for wallets.
+3. Delete route for wallets. 
+*/
+
 router.get('/', (req, res) => {
   const query = `SELECT * FROM "wallet";`;
   pool
