52b9cb2502a146728d395a12058bcec69caedf6a
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-08-12 03:45:58 -0500

    nav now returning null. Making separate branch to pull previous version before reset

--- components/Nav.js
+++ components/Nav.js
@@ -1,13 +1,6 @@
 import Link from 'next/link';
 
-/* To Do:
-1. Spike how to handle displays. Are different pages necessary or should different components be rendered at need
-*/
-
 const Nav = (props) => {
-  // taking in props for user image in top right corner
-  //later change
-
   return null;
 };
 
